301173fab55170e6df4195bcb042072052efb45d
Flynn Burklin <fburklin@us.ci.org>
2021-06-21 14:44:03 -0600

    Test drove search functionality, not appearance

--- src/App.js
+++ src/App.js
@@ -1,15 +1,54 @@
-const cars = ['car 1', 'car 2', 'car 3', 'car 4', 'car 5'];
+import {useState} from "react";
+
+const cars = [
+    {make: 'Toyoda', model: 'Five Runner', year: 2002},
+    {make: 'Rhonda', model: 'Civil', year: 1989},
+    {make: 'Afford', model: 'S Court', year: 2010},
+    {make: 'Heavy', model: 'Hevvelle', year: 1993},
+    {make: 'Avoid', model: 'Pram', year: 2020}
+];
+
 function App() {
-  return (
-    <div className="App">
-      <header data-testid="header">Auriga Car Company</header>
-      {cars.map((car, index) => (
-        <li key={`list-item-${index}`} data-testid="list-item">
-          {car}
-        </li>
-      ))}
-    </div>
-  );
+    const [searchParams, setSearchParams] = useState({});
+    const [make, setMake] = useState('');
+    const [model, setModel] = useState('');
+    const [year, setYear] = useState(0);
+
+    const handleSearchSubmit = (e) => {
+        e.preventDefault();
+
+        setSearchParams({make, model, year});
+    }
+
+    return (
+        <div className="App">
+            <header data-testid="header">Auriga Car Company</header>
+            <form onSubmit={handleSearchSubmit}>
+                <label>
+                    Make:
+                    <input data-testid="make-search" name='make' value={make} onChange={e => setMake(e.target.value)} />
+                </label>
+                <label>
+                    Model:
+                    <input data-testid="model-search" name='model' value={model} onChange={e => setModel(e.target.value)} />
+                </label>
+                <label>
+                    Year:
+                    <input data-testid="year-search" name='year' value={year} onChange={e => setYear(parseInt(e.target.value, 10) || 0)} />
+                </label>
+                <button type='submit'>Search</button>
+            </form>
+            {cars
+                .filter(car => !searchParams.make || car.make === searchParams.make)
+                .filter(car => !searchParams.model || car.model === searchParams.model)
+                .filter(car => !searchParams.year || car.year === searchParams.year)
+                .map((car, index) => (
+                <li key={`list-item-${index}`} data-testid="list-item">
+                    {car.year} {car.make} {car.model}
+                </li>
+            ))}
+        </div>
+    );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,66 @@
-import { render, screen } from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
+
   it('should have a header', () => {
     render(<App />);
     const header = screen.getByTestId(/header/i);
     expect(header).toBeInTheDocument();
   });
+
   it('should have an inventory', () => {
     render(<App />);
     const listItems = screen.getAllByTestId(/list-item/i);
     expect(listItems).toHaveLength(5);
+  });
+
+  it('should have search form', () => {
+    render(<App />);
+    const makeField = screen.getByTestId(/make-search/i);
+    const modelField = screen.getByTestId(/model-search/i);
+    const yearField = screen.getByTestId(/year-search/i);
+    const submitButton = screen.getByText('Search');
+
+    expect(makeField).toBeInTheDocument();
+    expect(modelField).toBeInTheDocument();
+    expect(yearField).toBeInTheDocument();
+    expect(submitButton).toBeInTheDocument();
+  });
+
+  it('should limit list to make searched for', () => {
+    render(<App />);
+    const makeField = screen.getByTestId(/make-search/i);
+    const submitButton = screen.getByText('Search');
+
+    fireEvent.change(makeField, { target: { value: 'Toyoda'}});
+    fireEvent.click(submitButton);
+
+    const listItems = screen.getAllByTestId(/list-item/i);
+    expect(listItems).toHaveLength(1);
+  });
+
+  it('should limit list to model searched for', () => {
+    render(<App />);
+    const modelField = screen.getByTestId(/model-search/i);
+    const submitButton = screen.getByText('Search');
+
+    fireEvent.change(modelField, { target: { value: 'Five Runner'}});
+    fireEvent.click(submitButton);
+
+    const listItems = screen.getAllByTestId(/list-item/i);
+    expect(listItems).toHaveLength(1);
+  });
+
+  it('should limit list to year searched for', () => {
+    render(<App />);
+    const yearField = screen.getByTestId(/year-search/i);
+    const submitButton = screen.getByText('Search');
+
+    fireEvent.change(yearField, { target: { value: 1989}});
+    fireEvent.click(submitButton);
+
+    const listItems = screen.getAllByTestId(/list-item/i);
+    expect(listItems).toHaveLength(1);
   });
 });

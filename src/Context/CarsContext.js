64787265a88a552f0804a91a1db2ef9d1d6065d1
Mike Flores <mflores@us.ci.org>
2021-06-23 13:59:30 -0700

    removed loading prop

--- src/Context/CarsContext.js
+++ src/Context/CarsContext.js
@@ -1,25 +1,22 @@
-import {createContext, useContext, useEffect, useState} from 'react'
-import {getCars} from "../Data/cars";
+import { createContext, useContext, useEffect, useState } from 'react';
+import { getCars } from '../Data/cars';
 
-const carsProvider = createContext(undefined)
-export const useCars = () => useContext(carsProvider)
+const carsProvider = createContext(undefined);
+export const useCars = () => useContext(carsProvider);
 
-export const CarsContext = ({children, value}) => {
-    const [loading, setLoading] = useState(!value)
-    const [cars, setCars] = useState(value || [])
+export const CarsContext = ({ children, value }) => {
+  console.log('RENDERING CarsContext');
+  const [cars, setCars] = useState(value || []);
 
-    useEffect(() => {
-        if (!value) {
-            getCars().then(cars => {
-                setLoading(false)
-                setCars(cars)
-            })
-        }
-    }, [value])
+  useEffect(() => {
+    if (!value) {
+      getCars().then((cars) => {
+        setCars(cars);
+      });
+    }
+  }, [value]);
 
-    return (
-        <carsProvider.Provider value={{loading, cars}}>
-            {children}
-        </carsProvider.Provider>
-    )
-}
+  return (
+    <carsProvider.Provider value={{ cars }}>{children}</carsProvider.Provider>
+  );
+};
